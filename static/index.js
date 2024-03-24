e9fc4d5c37c8ded79da19b8170bf6bebef8f81f0
chrisdaid <c.chrisdb@gmail.com>
2024-03-24 10:59:23 -0400

    added dashboard

--- static/index.js
+++ static/index.js
@@ -1,29 +1,60 @@
-import { createClient } from "@propelauth/javascript";
+console.log("running index.js on frontend");
 
-const authClient = createClient({
-    authUrl: "https://819084454.propelauthtest.com", // Replace with your actual Auth URL
-    enableBackgroundTokenRefresh: true,
+// big chart - historical (day) energy production and consumption
+// two lines
+
+const ctx5 = document.getElementById("canvas5");
+
+new Chart(ctx5, {
+  type: "line",
+  data: {
+    labels: ["01", "02", "03", "04", "05", "06"],
+    datasets: [
+      {
+        label: "Consumed",
+        data: [1, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        fill: true,
+        backgroundColor: "#2f93a270",
+        tension: 0.3,
+      },
+      {
+        label: "Produced",
+        data: [10, 5, 11, 6, 9, 2],
+        borderWidth: 1,
+        fill: true,
+        backgroundColor: "#04318b6e",
+        tension: 0.5,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+
+    scales: {
+      y: {
+        display: false,
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
 });
 
-// Hook up buttons to redirect to signup, login, etc
-document.getElementById("signup").onclick = authClient.redirectToSignupPage;
-document.getElementById("login").onclick = authClient.redirectToLoginPage;
-document.getElementById("account").onclick = authClient.redirectToAccountPage;
-document.getElementById("logout").onclick = authClient.logout;
-
-// When the logged in status changes, display one of the divs
-authClient.addLoggedInChangeObserver((isLoggedIn) => {
-    if (isLoggedIn) {
-        document.getElementById("display-when-logged-in").style.display = "revert";
-        document.getElementById("display-when-logged-out").style.display = "none";
-
-        // Get authentication info and set email to it
-        authClient.getAuthenticationInfoOrNull()
-            .then(authInfo => {
-                document.getElementById("email").innerText = authInfo?.user?.email;
-            });
-    } else {
-        document.getElementById("display-when-logged-in").style.display = "none";
-        document.getElementById("display-when-logged-out").style.display = "revert";
+// fetch data
+fetch("/get-data")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
     }
-});
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    document.getElementById("someElement").textContent = data.name;
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
